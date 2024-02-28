53aa0491c65ebf791bfa2b9fafd8e887914432ee
Yushi23 <ayushiev2014@mail.ru>
2024-02-28 23:53:39 +0800

    Update router

--- public/index.php
+++ public/index.php
@@ -16,9 +16,9 @@ $app->get('/', function ($request, $response) {
 //добавление новых маршрутов
 //GET /users и POST /users
 //разные маршруты со своими обработчиками.
-$app->get('/users', function ($request, $response) {
-    return $response->write('GET /users');
-});
+// $app->get('/users', function ($request, $response) {
+//     return $response->write('GET /users');
+// });
 
 $app->post('/users', function ($request, $response) {
     return $response->write('POST /users');
@@ -28,5 +28,10 @@ $app->post('/users', function ($request, $response) {
 // $app->post('/users', function ($request, $response) {
 //     return $response->withStatus(302);
 // });
+
+$app->get('/courses/{id}', function ($request, $response, array $args) {
+    $id = $args['id'];
+    return $response->write("Course id: {$id}");
+});
 
 $app->run();
